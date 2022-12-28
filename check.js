c8be2ce0ed1debcafd44ddf5ac2412d7230bf0a0
hugminsub <hugon.grabek@gmail.com>
2022-12-28 13:08:09 +0100

    wip

---
+++ check.js
@@ -0,0 +1,16 @@
+document.getElementById("check").addEventListener("click", function(){
+	if (document.getElementById("b2").checked.value 
+			alert("Dobrze")
+		}
+			else 
+	{
+			alert("źle")
+	}
+})
+
+
+
+
+
+
+
