670df1255ada454fd61edd2389ac5dadf645c710
hugminsub <hugon.grabek@gmail.com>
2023-01-10 21:56:04 +0100

    wip

--- check.js
+++ check.js
@@ -1,12 +1,12 @@
-document.getElementById("check").addEventListener("click", sprawdzanie, sprawdzanie2);
+document.getElementById("check").addEventListener("click", sprawdzanie);
 function sprawdzanie() {
-if (document.getElementById("b2").checked){ 
+if (document.getElementById("a1", "b2").checked){ 
 	alert("Dobrze")
 } else {
 	alert("źle");
 }
 }
-
+/*
 function sprawdzanie2() {
 if (document.getElementById("a1").checked){
 	alert("Dobrze")
@@ -14,3 +14,4 @@ if (document.getElementById("a1").checked){
 	alert("źle");
 }
 }
+*/
