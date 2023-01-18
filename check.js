ab7ce99acf52149a0b0b3f5432d859f6b6ae0d3d
hugminsub <hugon.grabek@gmail.com>
2023-01-18 21:39:34 +0100

    wip

--- check.js
+++ check.js
@@ -7,13 +7,38 @@ let b1 = document.getElementById("b1");
 let b2 = document.getElementById("b2");
 let b3 = document.getElementById("b3");
 let b4 = document.getElementById("b4");
+let c1 = document.getElementById("c1");
+let c2 = document.getElementById("c2");
+let c3 = document.getElementById("c3");
+let c4 = document.getElementById("c4");
 function sprawdzanie() {
 	document.getElementById("good").style.display= "none";
 	document.getElementById("bad").style.display= "none";
 
-if(a4.checked&& b3.checked && !a1.checked && !a2.checked && !a3.checked && !b1.checked && !b2.checked && !b4.checked ){ 
+if (aiscorrect() &&  biscorrect() && ciscorrect()){
 	document.getElementById("good").style.display= "block";
 } else {
 	document.getElementById("bad").style.display= "block";
+}
+}
+function aiscorrect(){
+	if(!a1.checked && !a2.checked && !a3.checked && a4.checked){
+	return true
+}else{
+	return false
+}
+}
+function biscorrect(){
+	if(!b1.checked && !b2.checked && b3.checked && !b4.checked){
+	return true
+}else{
+	return false
+}
+}
+function ciscorrect(){
+	if(c1.checked && c2.checked && c3.checked & c4.checked){
+	return true
+}else{
+	return false
 }
 }
